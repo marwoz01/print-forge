7f285475a5c3ca9ee2b7831a124d888e8ad1f872
Marcel <marwozniak01@gmail.com>
2026-02-23 16:44:26 +0100

    fix: update image URLs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,20 @@
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
+import PFLogoIcon from "@/public/printforge-logo-icon.svg"
+import PFLogo from "@/public/printforge-logo.svg"
+
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates"
+})
 
 export default function RootLayout({
   children,
@@ -7,26 +23,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
             <div className="relative">
-              {/* Desktop logo */}
+              {/* Desktop Logo */}
               <img
-                src="/printforge-logo.svg"
+                src={PFLogo.src}
                 alt="PrintForge Logo"
                 className="w-[200px] h-auto hidden md:block"
               />
-              {/* Mobile logo */}
+              {/* Mobile Logo */}
               <img
-                src="/printforge-logo-icon.svg"
+                src={PFLogoIcon.src}
                 alt="PrintForge Logo"
                 className="w-[40px] h-auto block md:hidden"
               />
             </div>
             <ul className="flex items-center gap-2.5">
               <p>3D Models</p>
-              <p>About</p>
+              <p href="/about">About</p>
             </ul>
           </nav>
         </header>
